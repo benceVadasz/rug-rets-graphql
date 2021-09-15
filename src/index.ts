85f7fe519728c0f691c1f34de5bc8f8b1b535dfb
vadaszbence <64799151+vadaszbence@users.noreply.github.com>
2021-09-15 17:05:23 +0200

    Changes sapollo server

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,47 @@
-import {ApolloServer} from "apollo-server-express";
 import express from 'express';
 import mongoose from 'mongoose'
 import cors from "cors";
 import {Mutation} from './resolvers/Mutation'
 import {Query} from './resolvers/Query'
+import {Subscription} from "./resolvers/Subscription";
 import {typeDefs} from "./typeDefs";
 import {getUserId} from "./utils";
+import {PubSub, ApolloServer} from 'apollo-server';
 
 const resolvers = {
     Mutation,
-    Query
+    Query,
+    Subscription
 }
 
+const app = express();
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: "50mb", extended: true}))
+app.use(cors());
 
-const startServer = async () => {
-    const app = express();
-    app.use(express.json({limit: '50mb'}));
-    app.use(express.urlencoded({limit: "50mb", extended: true}))
-    app.use(cors());
+const pubsub = new PubSub()
 
 
-    const server = new ApolloServer({
-        typeDefs,
-        resolvers,
-        context: async ({req}) => {
-            return {
-                ...req,
-                userId:
-                    req && req.headers.authorization && req.headers.authorization !== "undefined" ? getUserId(req) : null
-            };
-        }
-    });
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({req}) => {
+        return {
+            ...req,
+            pubsub,
+            userId:
+                req && req.headers.authorization && req.headers.authorization !== "undefined" ? getUserId(req) : null
+        };
+    }
+});
 
-    server.applyMiddleware({app});
+const CONNECTION_URL = "mongodb+srv://vadaszbence:EDpvmmkb439qYee@cluster0.x44no.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
-    const CONNECTION_URL = "mongodb+srv://vadaszbence:EDpvmmkb439qYee@cluster0.x44no.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-    await mongoose.connect(CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+mongoose.set('useFindAndModify', false);
 
-    mongoose.set('useFindAndModify', false);
-
-    app.listen({port: 4000}, () =>
-        console.log(`🚀 Server ready at http://localhost:4000/graphql`)
-    );
-};
-
-startServer()
-    .catch((e) => {
-        console.log(e)
-    })
+server.listen().then(({ url }: {url: string}) => console.log(`server started at ${url}`));
